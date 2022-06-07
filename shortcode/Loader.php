5289b54cf13818c48d6628b91cfa2e39680c402f
zeera <rspc.dev@gmail.com>
2022-06-07 22:39:56 +0800

    fix table layout

--- shortcode/Loader.php
+++ shortcode/Loader.php
@@ -28,11 +28,12 @@ class Loader
         // add_shortcode('wpplugin_bigexample', [self::class, "bigExample"]);
         $version = date("ymd-Gis", filemtime(WPDIGITALDRIVE_COMPETITIONS_PATH . 'assets/css/featured-competitions.css'));
         wp_register_style('featured-competition-style', WPDIGITALDRIVE_COMPETITIONS_URL . 'assets/css/featured-competitions.css?v=' . $version);
-        wp_register_style('entry-lists-style', WPDIGITALDRIVE_COMPETITIONS_URL . 'assets/css/entry-lists.css?v=' . $version);
-        wp_register_style('bootstrap-style', WPDIGITALDRIVE_COMPETITIONS_URL . 'assets/css/bootstrap.min.css?v=' . $version);
         wp_register_style('entrylists-datatable-style', WPDIGITALDRIVE_COMPETITIONS_URL . 'assets/css/dataTables.min.css?v=' . $version, true);
+        wp_register_style('entry-lists-style', WPDIGITALDRIVE_COMPETITIONS_URL . 'assets/css/entry-lists.css?v=' . $version);
+        wp_register_style('bootstrap-style', WPDIGITALDRIVE_COMPETITIONS_URL . 'assets/css/bootstrap.min.css?v=' . $versio, truen);
 
         wp_register_script('featured-competition-scripts', WPDIGITALDRIVE_COMPETITIONS_URL . 'assets/js/featured-competitions.js?v=' . $version, array('jquery'), '', true);
+        wp_register_script('bootstrap-scripts', WPDIGITALDRIVE_COMPETITIONS_URL . 'assets/js/bootstrap.bundle.min.js?v=' . $version, array('jquery'), '', true);
         wp_register_script('datatable-scripts', WPDIGITALDRIVE_COMPETITIONS_URL . 'assets/js/dataTables.min.js?v=' . $version, array('jquery'), '', true);
         wp_register_script('datatable-bootstrap-script', WPDIGITALDRIVE_COMPETITIONS_URL . 'assets/js/dataTables.bootstrap5.min.js?v=' . $version, array('jquery'), '', true);
 
@@ -99,6 +100,9 @@ class Loader
         }
         if(!wp_script_is('datatable-bootstrap-script')) {
             wp_enqueue_script("datatable-bootstrap-script");
+        }
+        if(!wp_script_is('bootstrap-scripts')) {
+            wp_enqueue_script("bootstrap-scripts");
         }
         //A useful function to know
         $attr = shortcode_atts(array(
